9cba77e9f82a3c45600035a48e882135701b4cee
imanebahiaoui <bahiaoui.imane@gmail.com>
2022-03-19 10:28:44 +0100

    UI

--- galleryviewer/galleryviewer/settings.py
+++ galleryviewer/galleryviewer/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'galleryviewer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'viewer',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
